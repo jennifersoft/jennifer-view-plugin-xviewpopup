279b465208100af9a7f542a2c21202d1cb9f4f59
alvin <seogi777@gmail.com>
2019-07-09 16:14:18 +0900

    Plugin Version changed.

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -12,11 +12,10 @@ jq(function() {
 
     setTimeout(function() {
         var txids = jq("#frm_plugin").find("input[name=txids]").val();
-        var sid = jq("#frm_plugin").find("input[name=sid]").val();
         var stime = jq("#frm_plugin").find("input[name=stime]").val();
         var etime = jq("#frm_plugin").find("input[name=etime]").val();
 
-        if(!txids || !sid || !stime || !etime) {
+        if(txids === "" || stime === "" || etime === "") {
             jq("#xviewpopup-main").find(".message").html("All parameters are required.");
         } else {
             jq("#xviewpopup-main").find("#frm_plugin").attr("action", extension.options.hostName + "/popup/xviewPointList").submit();
