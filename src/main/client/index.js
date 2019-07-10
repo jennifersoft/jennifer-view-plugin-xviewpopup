707053a3a4e6b15ef3288d2763c04e65b4280b23
alvin <seogi777@gmail.com>
2019-07-10 10:29:37 +0900

    Revert "Plugin Version changed."
    This reverts commit 279b4652

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -12,10 +12,11 @@ jq(function() {
 
     setTimeout(function() {
         var txids = jq("#frm_plugin").find("input[name=txids]").val();
+        var sid = jq("#frm_plugin").find("input[name=sid]").val();
         var stime = jq("#frm_plugin").find("input[name=stime]").val();
         var etime = jq("#frm_plugin").find("input[name=etime]").val();
 
-        if(txids === "" || stime === "" || etime === "") {
+        if(!txids || !sid || !stime || !etime) {
             jq("#xviewpopup-main").find(".message").html("All parameters are required.");
         } else {
             jq("#xviewpopup-main").find("#frm_plugin").attr("action", extension.options.hostName + "/popup/xviewPointList").submit();
