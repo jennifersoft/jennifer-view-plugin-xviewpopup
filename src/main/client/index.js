cf221dd2e71f762aec00bc840c4fa255d7c9b5db
alvin <seogi777@gmail.com>
2020-11-26 13:53:49 +0900

    David이 작업한 코드 반영

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -13,13 +13,36 @@ jq(function() {
     setTimeout(function() {
         var txids = jq("#frm_plugin").find("input[name=txids]").val();
         var sid = jq("#frm_plugin").find("input[name=sid]").val();
-        var stime = jq("#frm_plugin").find("input[name=stime]").val();
-        var etime = jq("#frm_plugin").find("input[name=etime]").val();
+        var stime = jq("#frm_plugin").find("input[name=startTime]").val();
+        var etime = jq("#frm_plugin").find("input[name=endTime]").val();
+
+        const transactionIds = JSON.stringify({ [sid]: [txids] } )
+        jq("#frm_plugin").append(
+            "<input type='hidden' name='transactionIds' value='" + transactionIds + "' />"
+        );
+
+        const emptyArray = JSON.stringify([]);
+        const emptyObject = JSON.stringify({});
+        jq("#frm_plugin").append(
+            "<input type='hidden' name='transactionCount' value='1' />"
+        );
+        jq("#frm_plugin").append(
+            "<input type='hidden' name='transactionRows' value='" + emptyArray + "' />"
+        );
+
+        jq("#frm_plugin").append(
+            "<input type='hidden' name='groupRows' value='"+emptyArray+"' />"
+        );
+
+        jq("#frm_plugin").append(
+            "<input type='hidden' name='instanceOids' value='"+ emptyObject+"' />"
+        );
+
 
         if(!txids || !sid || !stime || !etime) {
             jq("#xviewpopup-main").find(".message").html("All parameters are required.");
         } else {
-            jq("#xviewpopup-main").find("#frm_plugin").attr("action", extension.options.hostName + "/popup/xviewPointList").submit();
+            jq("#xviewpopup-main").find("#frm_plugin").attr("action", extension.options.hostName + "/popup/xviewAnalysis").submit();
         }
     }, 1000);
 });
